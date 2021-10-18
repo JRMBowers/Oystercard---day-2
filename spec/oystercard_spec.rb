859202e1207828908e5e135f2254c82bd37e3e67
Patrick Barsch <PatrickGB@gmx.de>
2021-10-18 16:47:20 +0200

    task #5 - #top_up allows us, to increase our balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,17 @@
 require 'oystercard'
+
 describe Oystercard do 
+
   it 'has a balance' do
     expect(subject.balance).to eq (0)
   end  
 
+  describe '#top_up' do
+
+    it 'allows us, to increase our balance' do 
+      expect { subject.top_up(20) }.to change { subject.balance }.by 20 
+    end
+
+  end
 
 end 
